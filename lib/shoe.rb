30efd035d284abb1fbfa9fd95f32c4e3cddc9f2b
Thanh Pham <tpham0123@gmail.com>
2020-05-21 16:21:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,7 +10,7 @@ class Shoe
   def cobble 
      
     puts "Your shoe is as good as new!"
-    @condition= "new"
+    @condition= "new"         #what is it doing here? instancitating?
   end 
   
 end 
